079a837528639afd7fe67763a066486a26a87a1b
Romain <RomainFrechic@outlook.fr>
2016-11-07 12:28:52 +0100

    formulaire

---
+++ serveur.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs =require('fs');
+
+// Crée l'application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static('public'));
+app.use(urlencodedParser);
+
+app.post('/process_post',function (req, res) {
+   // Préparer une sortie au format JSON
+   console.log(req.body);
+   response = {
+      path:"/"+req.body.path+".md",
+      title:req.body.title
+   };
+
+
+
+
+   fs.readFile(__dirname +'/public/crm.json', 'utf8',function(err,data){
+      //transformer en objet
+      var content = JSON.parse(data);
+      //mettre dans le menuJSON
+      content.menu.push(response);
+      //transformer en string
+      var fileStr = JSON.stringify(content);
+      if(err) throw err;
+
+      fs.writeFile(__dirname +'/public/crm.json',fileStr, 'utf8',function(err){
+         if(err) throw err;
+         
+      });
+   });
+fs.writeFile(__dirname+'/public/'+req.body.path+".md",req.body.file,'utf8',function(err){
+   if(err) throw err;
+   res.send('ok');
+});
+});
+
+
+var server = app.listen(2000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+
+});
+
+
+
